df372948880682bbdabda5ad794bcc000d9ce9c5
Jeremiah-Katumo <jeremykatush@gmail.com>
2023-04-25 13:47:32 +0300

    leap years

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+	const newArray = [];
+	array.forEach((item) => {
+		if (!args.push(item));
+	});
+	return newArray;
 };
 
 // Do not edit below this line
